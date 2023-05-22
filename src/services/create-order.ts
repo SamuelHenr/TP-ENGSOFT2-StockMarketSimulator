4e454476d9270b5ad6702de496ea534ccbb700f7
Gabriel Fialho <gabrielmmfialho@hotmail.com>
2023-05-21 22:22:25 -0300

    Refatoração da função matchOrder

--- src/services/create-order.ts
+++ src/services/create-order.ts
@@ -21,29 +21,46 @@ const insertAndSortOrders = (orders: Order[], order: Order) => {
   orders.sort((order1, order2) => order1.price - order2.price)
 }
 
-const matchOrder = ({ buyOrders, sellOrders, fulfilledOrders, order }: { buyOrders: Order[], sellOrders: Order[], fulfilledOrders: Order[], order: Order }) => {
-  if (order.quantityRemaining === 0) {
+const matchOrder = (orderRequest: CreateOrderRequest) => {
+  if (orderRequest.order.quantityRemaining === 0) {
     return
   }
-  const isBuy = order.side === OrderSide.BUY
-  const matchingOrderList = isBuy ? sellOrders : buyOrders
+
+  const matchingOrderList = retrieveOrderList(orderRequest)
   const matchingOrder = matchingOrderList.pop()
   if (matchingOrder == null) {
     return
   }
-  if (!(isBuy
-    ? matchingOrder.price <= order.price
-    : order.price <= matchingOrder.price)) {
+
+  if (isOrdersMatching(matchingOrder, orderRequest)) {
     matchingOrderList.push(matchingOrder)
     return
   }
-  const filledQuantity = Math.min(matchingOrder.quantityRemaining, order.quantityRemaining)
-  order.quantityRemaining -= filledQuantity
+
+  const filledQuantity = Math.min(matchingOrder.quantityRemaining, orderRequest.order.quantityRemaining)
+  orderRequest.order.quantityRemaining -= filledQuantity
   matchingOrder.quantityRemaining -= filledQuantity
   if (matchingOrder.quantityRemaining > 0) {
     matchingOrderList.push(matchingOrder)
   } else {
-    fulfilledOrders.push(matchingOrder)
+    orderRequest.fulfilledOrders.push(matchingOrder)
   }
-  matchOrder({ buyOrders, sellOrders, fulfilledOrders, order })
+  matchOrder(orderRequest)
+}
+
+const retrieveOrderList = (orderRequest: CreateOrderRequest): Order[] => {
+  return orderRequest.order.side === OrderSide.BUY ? orderRequest.sellOrders : orderRequest.buyOrders
+}
+
+const isOrdersMatching = (lastOrder: Order, orderRequest: CreateOrderRequest) => {
+  if (orderRequest.order.side === OrderSide.BUY) {
+    if (lastOrder.price <= orderRequest.order.price) {
+      return false
+    }
+  } else {
+    if (lastOrder.price >= orderRequest.order.price) {
+      return false
+    }
+  }
+  return true
 }

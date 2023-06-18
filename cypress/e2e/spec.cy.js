0720cb587d01b2be4c9b1f69be9cce3b6c3115bc
Gabriel Fialho <gabrielmmfialho@hotmail.com>
2023-06-18 17:38:15 -0300

    more cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,13 @@
-describe('Verificar opções do select "side"', () => {
+function getTable(n) {
+  return cy.get('.orders-table').eq(n).find('tbody').find('tr');
+}
+
+describe('Testes de sistema', () => {
   beforeEach(() => {
     cy.visit('localhost:3000');
   });
 
-  it('Deve conter as opções "BUY" e "SELL"', () => {
+  it(' "select #side" Deve conter as opções "Buy" e "Sell"', () => {
     cy.get('#side').should('exist');
     cy.get('#side').should('have.prop', 'tagName', 'SELECT');
 
@@ -12,4 +16,49 @@ describe('Verificar opções do select "side"', () => {
     cy.get('#side').should('contain', 'Buy');
     cy.get('#side').should('contain', 'Sell');
   });
+
+  it('Compra gera uma nova linha na tabela de compras', () => {
+    const price = '10'; // Valor para o campo "price"
+    const quantity = '20'; // Valor para o campo "quantity"
+
+    cy.get('input[name="price"]').type(price); // Insere o valor no campo "price"
+    cy.get('input[name="quantity"]').type(quantity); // Insere o valor no campo "quantity"
+    cy.get('#side').select('Buy'); // Seleciona a opção "BUY" no select com id "side"
+    cy.get('form').submit(); // Envia o formulário 
+
+    getTable(0).should('have.length', 1);
+  });
+
+  it('Venda gera uma nova linha na tabela de vendas', () => {
+    const price = '10'; // Valor para o campo "price"
+    const quantity = '20'; // Valor para o campo "quantity"
+
+    cy.get('input[name="price"]').type(price); // Insere o valor no campo "price"
+    cy.get('input[name="quantity"]').type(quantity); // Insere o valor no campo "quantity"
+    cy.get('#side').select('Sell'); // Seleciona a opção "BUY" no select com id "side"
+    cy.get('form').submit(); // Envia o formulário 
+
+    getTable(1).should('have.length', 1);
+  });
+
+
+  it('Compras e vendas de mesmo valor e quantidade se anulam', () => {
+    const price = '10'; // Valor para o campo "price"
+    const quantity = '20'; // Valor para o campo "quantity"
+
+    cy.get('input[name="price"]').type(price); // Insere o valor no campo "price"
+    cy.get('input[name="quantity"]').type(quantity); // Insere o valor no campo "quantity"
+    cy.get('#side').select('Buy'); // Seleciona a opção "BUY" no select com id "side"
+    cy.get('form').submit(); // Envia o formulário  
+
+    cy.get('input[name="price"]').clear().type(price); // Insere o valor no campo "price"
+    cy.get('input[name="quantity"]').clear().type(quantity); // Insere o valor no campo "quantity"
+    cy.get('#side').select('Sell'); // Seleciona a opção "BUY" no select com id "side"
+    cy.get('form').submit(); // Envia o formulário
+
+    getTable(0).should('have.length', 0);
+    getTable(1).should('have.length', 0);
+    getTable(2).should('have.length', 2);
+  });
+
 });

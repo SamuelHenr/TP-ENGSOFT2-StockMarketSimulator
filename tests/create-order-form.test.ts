b96f46fa6ad06513335ea15c3e484f9961bf04a8
Gabriel Torres Bolognani <gabrieltorres@dcc.ufmg.br>
2023-06-19 00:15:23 +0000

    unit tests components

---
+++ tests/create-order-form.test.ts
@@ -0,0 +1,55 @@
+import { render, fireEvent, screen } from "@testing-library/react"
+import { useForm } from "react-hook-form"
+import CreateOrderForm from "../src/components/components/create-order-form"
+import OrderSide from "../src/interfaces/order/order-side"
+
+jest.mock("react-hook-form", () => ({
+  ...jest.requireActual("react-hook-form"),
+  useForm: jest.fn(),
+}));
+
+
+jest.mock("react-hook-form", () => ({
+    ...jest.requireActual("react-hook-form"),
+    useForm: jest.fn(),
+  }));
+  
+  describe("CreateOrderForm", () => {
+    const mockHandleSubmit = jest.fn((data) => data);
+    const mockRegister = jest.fn();
+  
+    beforeEach(() => {
+      (useForm as jest.Mock).mockReturnValue({
+        register: mockRegister,
+        handleSubmit: mockHandleSubmit,
+      });
+    });
+  
+    test("Should render correctly", () => {
+      const mockProps = {
+        buyOrders: [],
+        sellOrders: [],
+        fulfilledOrders: [],
+        setBuyOrders: jest.fn(),
+        setSellOrders: jest.fn(),
+        setFulfilledOrders: jest.fn(),
+      };
+
+
+      render(<CreateOrderForm {...mockProps} />);
+      expect(screen.getByLabelText("Price:")).toBeInTheDocument();
+      expect(screen.getByLabelText("Quantity:")).toBeInTheDocument();
+      expect(screen.getByLabelText("Side:")).toBeInTheDocument();
+    });
+  
+    test("Should call handleSubmit on form submission", () => {
+      const mockProps = {
+        setBuyOrders: jest.fn(),
+        setSellOrders: jest.fn(),
+        setFulfilledOrders: jest.fn(),
+      };
+      render(<CreateOrderForm {...mockProps} />);
+      fireEvent.submit(screen.getByText("Submit"));
+      expect(mockHandleSubmit).toHaveBeenCalled();
+    });
+  });
